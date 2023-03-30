43ef31e54157a55d4254fe1bd38365b91e760ba4
Taehyun <taehyunjo97@gmail.com>
2023-03-30 15:54:55 +0900

    Update ContentView.swift

--- git-session-sample.swiftpm/ContentView.swift
+++ git-session-sample.swiftpm/ContentView.swift
@@ -8,6 +8,7 @@ struct ContentView: View {
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
             Text("Commit!")
+            Text("Commit!!")
         }
     }
 }
